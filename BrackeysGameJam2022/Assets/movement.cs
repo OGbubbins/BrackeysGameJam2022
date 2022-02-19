2a1656a25e96e5758928823ed998a2b048a07709
OGbubbins <99917746+OGbubbins@users.noreply.github.com>
2022-02-19 22:04:27 +0000

    Fixed the character controller
    used a character controller instead of changing the transform

--- BrackeysGameJam2022/Assets/movement.cs
+++ BrackeysGameJam2022/Assets/movement.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
 
+    public CharacterController Controller;
+
     public float turnspeed;
     public float movespeed;
     
     void Update()
     {
-        transform.position += transform.forward * Input.GetAxis("Vertical") * movespeed * Time.deltaTime;
-        transform.rotation = transform.rotation * Quaternion.Euler(0f, Input.GetAxis("Horizontal") * turnspeed * Time.deltaTime, 0f);
         
+        transform.rotation = transform.rotation * Quaternion.Euler(0f, Input.GetAxis("Horizontal") * turnspeed * Time.deltaTime, 0f);
+        Controller.Move(transform.forward * Input.GetAxis("Vertical") * movespeed * Time.deltaTime);
+        
+
+
     }
 }

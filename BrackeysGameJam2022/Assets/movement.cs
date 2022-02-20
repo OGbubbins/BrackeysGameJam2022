bac0f4a38379c5bcc5deaba4add92ebac4674bf3
E-RIDER2000 <99980612+E-RIDER2000@users.noreply.github.com>
2022-02-20 16:41:17 +0000

    added first person controller
    it goated and is first person with a controller and floor and slope amazing

--- BrackeysGameJam2022/Assets/movement.cs
+++ BrackeysGameJam2022/Assets/movement.cs
@@ -4,14 +4,41 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    [SerializeField] CharacterController controller;
+    [SerializeField] float speed;
+    Vector3 velocity;
+    float gravity = -9.8f;
+    float jumpHeight = 2f;
+    bool isGrounded;
 
-    public float turnspeed;
-    public float movespeed;
-    
+    [SerializeField] Transform groundCheck;
+    float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Input.GetAxis("Vertical") * movespeed * Time.deltaTime;
-        transform.rotation = transform.rotation * Quaternion.Euler(0f, Input.GetAxis("Horizontal") * turnspeed * Time.deltaTime, 0f);
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); //makes an invisible sphere which checks if anything in the ground mask is in it
+        if (isGrounded == true && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity); // to reach the height of h     formula  is v = sqrt(h * -2 * gravity) 
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;  //simulating gravity
+        controller.Move(velocity * Time.deltaTime);
     }
 }

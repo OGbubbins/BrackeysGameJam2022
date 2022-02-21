7250fdb9c9f26d813adeebcf97d94e1d4a1d2e37
E-RIDER2000 <99980612+E-RIDER2000@users.noreply.github.com>
2022-02-21 15:45:07 +0000

    end of level trigger
    sends you to next level as said in build settings

---
+++ BrackeysGameJam2022/Assets/scripts/endLevelTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endLevelTrigger : MonoBehaviour
+{
+    private void loadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        loadNextScene();
+    }
+}
